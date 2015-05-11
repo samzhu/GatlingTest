f73e16e79235ff9ce13d09278b81353e85799b56
samzhu <spike19820318@gmail.com>
2015-05-12 03:04:57 +0800

    add comm

--- src/main/scala/samtest/AlertAdd.scala
+++ src/main/scala/samtest/AlertAdd.scala
@@ -4,10 +4,12 @@ import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import io.gatling.jdbc.Predef._
 
+//一定要繼承Simulation，格林機關槍才可以識別你的腳本
 class AlertAdd extends Simulation {
   val baseURL = "http://10.240.136.204:8080"
-  val csvFeeder = csv("alert_terms.csv").queue // use a comma separator
-
+  //測試資料來源為csv檔案,queue如果用完會出現Exception，如果你希望無限循環應該使用csv("alert_terms.csv").circular
+  val csvFeeder = csv("alert_terms.csv").queue
+  //基本的http設定
   val httpConf = http
     .baseURL(baseURL)
     .acceptCharsetHeader("utf-8;q=0.7,*;qc=0.7")
@@ -16,17 +18,22 @@ class AlertAdd extends Simulation {
     .acceptLanguageHeader("en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4")
     .disableFollowRedirect
 
+  //綁定Content-Type
   val header = Map(
-    "Keep-Alive" -> "115",
     "Content-Type" -> "application/json; charset=UTF-8")
 
+  //主要執行指令
+  //repeat表示我要執行的次數，這邊指定跟檔案一樣多筆才不會爆掉
   val scn = scenario("alertAdd").repeat(csvFeeder.records.size) {
     feed(csvFeeder)
       .exec(http("alertAdd")
       .post("/springstock/alertterms")
+      //Http Body為Json格式
       .body(StringBody( """{"cid": "${cid}","funcid": "${funcid}","tab": "${tab}","stk": "${stk}","val": "${val}","msgtype": "${msgtype}","expire": "${expire}","xcnts": "${xcnts}","cstamp": "${cstamp}","staff": "${staff}","idx": "${idx}"}""")).asJSON
-      .headers(header).check(status.is(200)))
+      .headers(header)
+      //check是成功條件
+      .check(status.is(200)))
   }
-
+  //注入模擬單一使用者
   setUp(scn.inject(atOnceUsers(1))).protocols(httpConf)
 }
--- src/main/scala/samtest/StockPost.scala
+++ src/main/scala/samtest/StockPost.scala
@@ -4,9 +4,9 @@ import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import io.gatling.jdbc.Predef._
 
-class StockPost extends Simulation{
-  val baseURL = "http://10.240.136.204:8080"
-  val duringTime:Int = 60*60*1
+class StockPost extends Simulation {
+  val baseURL = "http://127.0.0.1:8080"
+  val duringTime: Int = 60
   val csvAlert = csv("alert_terms.csv").circular
   val csvSemerging = csv("stock/emerging.txt").circular
   val csvSfut = csv("stock/fut.txt").circular
@@ -25,12 +25,16 @@ class StockPost extends Simulation{
   val header = Map(
     "Content-Type" -> "application/json; charset=UTF-8")
 
-  val alert = scenario("Alert").during(duringTime) {
+  //這邊我要模擬一筆資料刪除後又新增
+  //during(持續測試時間,單位是秒)
+  val alert = scenario("Alert").during(60) {
     feed(csvAlert)
+      //先執行新增
       .exec(http("AlertAdd")
       .post("/springstock/alertterms")
       .body(StringBody( """{"cid": "${cid}","funcid": "${funcid}","tab": "${tab}","stk": "${stk}","val": "${val}","msgtype": "${msgtype}","expire": "${expire}","xcnts": "${xcnts}","cstamp": "${cstamp}","staff": "${staff}","idx": "${idx}"}""")).asJSON
       .headers(header).check(status.is(200)))
+      //同一筆資料再執行刪除
       .exec(http("AlertDel")
       .delete("/springstock/alertterms?cid=${cid}&funcid=${funcid}&tab=${tab}&stk=${stk}&val=${val}&msgtype=${msgtype}")
       .headers(header).check(status.is(200)))
@@ -39,44 +43,44 @@ class StockPost extends Simulation{
     feed(csvSemerging)
       .exec(http("stock_emerging")
       .post("/springstock/stock")
-      .body(StringBody("""{"tab": "emerging","sid": "${sid}","strikeprice":"${d}" }""")).asJSON
+      .body(StringBody( """{"tab": "emerging","sid": "${sid}","strikeprice":"${d}" }""")).asJSON
       .headers(header).check(status.is(200)))
   }
   val scn_fut = scenario("stock_fut").during(duringTime) {
     feed(csvSfut)
       .exec(http("stock_fut")
       .post("/springstock/stock")
-      .body(StringBody("""{"tab": "fut","sid": "${sid}","strikeprice":"${d}" }""")).asJSON
+      .body(StringBody( """{"tab": "fut","sid": "${sid}","strikeprice":"${d}" }""")).asJSON
       .headers(header).check(status.is(200)))
   }
   val scn_opt = scenario("stock_opt").during(duringTime) {
     feed(csvSopt)
       .exec(http("stock_opt")
       .post("/springstock/stock")
-      .body(StringBody("""{"tab": "opt","sid": "${sid}","strikeprice":"${d}" }""")).asJSON
+      .body(StringBody( """{"tab": "opt","sid": "${sid}","strikeprice":"${d}" }""")).asJSON
       .headers(header).check(status.is(200)))
   }
   val scn_otc = scenario("stock_otc").during(duringTime) {
     feed(csvSotc)
       .exec(http("stock_otc")
       .post("/springstock/stock")
-      .body(StringBody("""{"tab": "otc","sid": "${sid}","strikeprice":"${d}" }""")).asJSON
+      .body(StringBody( """{"tab": "otc","sid": "${sid}","strikeprice":"${d}" }""")).asJSON
       .headers(header).check(status.is(200)))
   }
   val scn_tse = scenario("stock_tse").during(duringTime) {
     feed(csvStse)
       .exec(http("stock_tse")
       .post("/springstock/stock")
-      .body(StringBody("""{"tab": "tse","sid": "${sid}","strikeprice":"${d}" }""")).asJSON
+      .body(StringBody( """{"tab": "tse","sid": "${sid}","strikeprice":"${d}" }""")).asJSON
       .headers(header).check(status.is(200)))
   }
 
   setUp(alert.inject(atOnceUsers(1))
-    ,scn_emerging.inject(atOnceUsers(3))
-    ,scn_fut.inject(atOnceUsers(3))
-    ,scn_opt.inject(atOnceUsers(3))
-    ,scn_otc.inject(atOnceUsers(3))
-    ,scn_tse.inject(atOnceUsers(3))
+    , scn_emerging.inject(atOnceUsers(3))
+    , scn_fut.inject(atOnceUsers(3))
+    , scn_opt.inject(atOnceUsers(3))
+    , scn_otc.inject(atOnceUsers(3))
+    , scn_tse.inject(atOnceUsers(3))
   ).protocols(httpConf)
 
 
